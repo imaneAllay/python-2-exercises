17998eebc2c331c235937eef6472609a3fe7007d
Imane Allay <iallay@xpanxion.com>
2023-06-29 16:24:57 -0400

    Add ex3

--- main.py
+++ main.py
@@ -5,21 +5,29 @@ from src import solutions
 
 def main():
 
+#     people_list = [
+#     {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
+#     {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
+#     {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
+# ]
+
     people_list = [
-    {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
-    {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
-    {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
-]   
-    
+        {'id': 2, 'name': 'bob',     'weight_kg': 90, 'height_meters': 1.7},
+        {'id': 3, 'name': 'charlie', 'weight_kg': 80, 'height_meters': 1.8},
+    ]
+    # -----------------------------Ex1-----------------------------------
     # solutions.sort_people()
     # solutions.sort_people(people_list, 'name', 'desc')
     # print(people_list)
+    # -----------------------------Ex2-----------------------------------
+    #
+    # filtered_list = solutions.filter_males(people_list)
+    # print(filtered_list)
 
-    filtered_list = solutions.filter_males(people_list)
-    print(filtered_list)
+    # -----------------------------Ex3-----------------------------------
 
-
-
+    # new_people_list = solutions.calc_bmi(people_list)
+    # print(new_people_list)
 
 
 
--- src/solutions.py
+++ src/solutions.py
@@ -3,6 +3,22 @@ from pprint import pprint
 # def sort_people(people_list, field, direction):
 #     people_list.sort(key=lambda p: p[field], reverse= (direction == 'desc'))
 
-def filter_males(people_list):
-    return  list(filter(lambda p:p["sex"]=='male',people_list))
+# def filter_males(people_list):
+#     return  list(filter(lambda p:p["sex"]=='male',people_list))
 
+def transform_person(person):
+    weight = person['weight_kg']
+    height = person['height_meters']
+    bmi = round(weight / (height ** 2), 1)
+
+    p = {
+        'id': person['id'],
+        'name': person['name'],
+        'weight_kg': weight,
+        'height_meters': height,
+        'bmi': bmi
+    }
+    return p
+
+def calc_bmi(people_list):
+    return list(map( transform_person,people_list))
