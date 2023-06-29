baf2ec99be63a5ae488065a377f7dd9a497cb219
Imane Allay <iallay@xpanxion.com>
2023-06-29 16:37:55 -0400

    Add Ex 4

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from src import solutions
 
 def main():
 
-#     people_list = [
-#     {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
-#     {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
-#     {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
-# ]
-
     people_list = [
-        {'id': 2, 'name': 'bob',     'weight_kg': 90, 'height_meters': 1.7},
-        {'id': 3, 'name': 'charlie', 'weight_kg': 80, 'height_meters': 1.8},
-    ]
+    {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
+    {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
+    {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
+]
+
+    # people_list = [
+    #     {'id': 2, 'name': 'bob',     'weight_kg': 90, 'height_meters': 1.7},
+    #     {'id': 3, 'name': 'charlie', 'weight_kg': 80, 'height_meters': 1.8},
+    # ]
     # -----------------------------Ex1-----------------------------------
     # solutions.sort_people()
     # solutions.sort_people(people_list, 'name', 'desc')
@@ -28,6 +28,18 @@ def main():
 
     # new_people_list = solutions.calc_bmi(people_list)
     # print(new_people_list)
+
+    # -----------------------------Ex4-----------------------------------
+    # print(solutions.get_people(people_list))
+
+    # -----------------------------Ex5-----------------------------------
+    sentence = "This is a test of the emergency broadcast system"
+    word_counter = WordCounter(sentence)
+    word_counter.count_words()
+    print(word_counter.get_word_count())    # Returns the number of all the words.
+    print(word_counter.get_shortest_word()) # Returns the length of the shortest word.
+    print(word_counter.get_longest_word())  # Returns the length of the longest word.
+
 
 
 
--- src/solutions.py
+++ src/solutions.py
@@ -6,19 +6,28 @@ from pprint import pprint
 # def filter_males(people_list):
 #     return  list(filter(lambda p:p["sex"]=='male',people_list))
 
-def transform_person(person):
-    weight = person['weight_kg']
-    height = person['height_meters']
-    bmi = round(weight / (height ** 2), 1)
+# def transform_person(person):
+#     weight = person['weight_kg']
+#     height = person['height_meters']
+#     bmi = round(weight / (height ** 2), 1)
+#
+#     p = {
+#         'id': person['id'],
+#         'name': person['name'],
+#         'weight_kg': weight,
+#         'height_meters': height,
+#         'bmi': bmi
+#     }
+#     return p
+#
+# def calc_bmi(people_list):
+#     return list(map( transform_person,people_list))
 
-    p = {
-        'id': person['id'],
-        'name': person['name'],
-        'weight_kg': weight,
-        'height_meters': height,
-        'bmi': bmi
-    }
-    return p
 
-def calc_bmi(people_list):
-    return list(map( transform_person,people_list))
+# def get_people(people_list):
+#     return [p['name'] for p in people_list if p['age'] >= 15 ]
+
+
+class WordCounter:
+
+    def --
